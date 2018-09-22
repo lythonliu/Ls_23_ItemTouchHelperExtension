cae3dd452339f63d35743e7cb1f5573bf356349a
Lython <liu.junli@moxiangroup.com>
2018-09-22 10:47:16 +0800

    init

---
+++ app/src/main/java/com/dongnao/alvin/ls_23_itemtouchhelperextension/MainActivity.java
@@ -0,0 +1,53 @@
+package com.dongnao.alvin.ls_23_itemtouchhelperextension;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.dongnao.alvin.ls_23_itemtouchhelperextension.ItemTouchHelpExtend.ItemTouchHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends com.lythonliu.LinkAppCompatActivity {
+    @Override
+    public String getAppName(){
+        return BuildConfig.APP_NAME;
+    }
+
+    private RecyclerView mRecyclerView;
+    private MainRecyclerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new MainRecyclerAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this));
+        mAdapter.updateData(createTestDatas());
+
+        ItemTouchHelpCallback callback = new ItemTouchHelpCallback();
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    private List<TestModel> createTestDatas() {
+        List<TestModel> result = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            TestModel testModel= new TestModel(i,":Item Swipe Action Button Container Width");
+            if (i == 1) {
+                testModel = new TestModel(i, "Item Swipe with Action container width and no spring");
+            }
+            if (i == 2) {
+                testModel = new TestModel(i, "Item Swipe with RecyclerView Width");
+            }
+            result.add(testModel);
+        }
+        return result;
+    }
+}
